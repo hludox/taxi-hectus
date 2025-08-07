459cbdab3b1b7cf4571047cdc651e7b7ef363c84
hludox <133472934+hludox@users.noreply.github.com>
2025-08-07 01:37:06 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,13 +18,13 @@ export default function Home() {
         <p className="text-xl text-white text-center mb-8 max-w-2xl">Réservez votre taxi 24/7 dans toutes les communes de Guadeloupe. Confort, ponctualité, trajets aéroport, touristiques, locaux.</p>
         <Link href="/reservation" className="bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700">Réserver Maintenant</Link>
         <div className="mt-8 flex space-x-4">
-          <a href="tel:+590690523736" title="Appeler"><img src="/images/phone.svg" alt="Téléphone" className="w-8 h-8" /></a>
-          <a href="sms:+590690523736" title="Envoyer un SMS"><img src="/images/sms.svg" alt="SMS" className="w-8 h-8" /></a>
-          <a href="https://wa.me/0590690523736" title="WhatsApp"><img src="/images/whatsapp.svg" alt="WhatsApp" className="w-8 h-8" /></a>
-          <a href="mailto:hludox@gmail.com" title="Envoyer un e-mail"><img src="/images/email.svg" alt="E-mail" className="w-8 h-8" /></a>
-          <a href="https://www.facebook.com/sarltaxihectus" title="Facebook"><img src="/images/facebook.svg" alt="Facebook" className="w-8 h-8" /></a>
-          <a href="https://www.instagram.com/sarl_taxi_hectus" title="Instagram"><img src="/images/instagram.svg" alt="Instagram" className="w-8 h-8" /></a>
-          <a href="https://www.tripadvisor.fr/Attraction_Review-g3411234-d33368528-Reviews-Sarl_Taxi_Hectus-capesterre_belle_eau_Basse_Terre_Island_Guadeloupe.html" title="TripAdvisor"><img src="/images/tripadvisor.svg" alt="TripAdvisor" className="w-8 h-8" /></a>
+          <a href="tel:+590690523736" title="Appeler"><Image src="/images/phone.svg" alt="Téléphone" width={32} height={32} /></a>
+          <a href="sms:+590690523736" title="Envoyer un SMS"><Image src="/images/sms.svg" alt="SMS" width={32} height={32} /></a>
+          <a href="https://wa.me/0590690523736" title="WhatsApp"><Image src="/images/whatsapp.svg" alt="WhatsApp" width={32} height={32} /></a>
+          <a href="mailto:hludox@gmail.com" title="Envoyer un e-mail"><Image src="/images/email.svg" alt="E-mail" width={32} height={32} /></a>
+          <a href="https://www.facebook.com/sarltaxihectus" title="Facebook"><Image src="/images/facebook.svg" alt="Facebook" width={32} height={32} /></a>
+          <a href="https://www.instagram.com/sarl_taxi_hectus" title="Instagram"><Image src="/images/instagram.svg" alt="Instagram" width={32} height={32} /></a>
+          <a href="https://www.tripadvisor.fr/Attraction_Review-g3411234-d33368528-Reviews-Sarl_Taxi_Hectus-capesterre_belle_eau_Basse_Terre_Island_Guadeloupe.html" title="TripAdvisor"><Image src="/images/tripadvisor.svg" alt="TripAdvisor" width={32} height={32} /></a>
         </div>
       </div>
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
