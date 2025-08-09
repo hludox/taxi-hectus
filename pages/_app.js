a2b85e31a82c1c97d3eda3b288b14c1eb73b8dce
hludox <133472934+hludox@users.noreply.github.com>
2025-08-09 15:52:53 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '../styles/globals.css'
+// pages/_app.js
+import '../styles/globals.css';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google Analytics – charge après l'interaction */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-XXXXXXXXXX');`}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
